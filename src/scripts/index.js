fd78b01103912dfda11116af1778b023b06efa23
ijomah jacob <ijomahifinwa@gmail.com>
2023-02-01 10:16:51 -0800

    completed search

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,47 @@
 import '../styles/index.scss';
 
 console.log('webpack starterkit');
+
+// const imgUrl = "fill:///storage/emulated/0/Android/data/net.bincom.cates.slp/files/media-content-id-2128/12434453454Mypic.png";
+
+// let arrItems = imgUrl.split('/');
+//  console.log(arrItems);
+
+//  let imgFolderName = arrItems[arrItems.length - 2];
+//  let imgFolderNameArr = imgFolderName.split('-');
+//  console.log(imgFolderNameArr);
+
+//  let imgIdString = imgFolderNameArr[imgFolderNameArr.length-1];
+//  console.log(typeof(Number(imgIdString)));
+// let imgId = Number(imgIdString);
+// console.log(typeof(imgId));
+
+
+// function submitForm() {
+//     let formDetails = document.querySelector('#myForm').submit();
+//     console.log("formDet", formDetails);
+
+//     let editDiv = document.querySelector('#editdiv');
+//     console.log(editDiv);
+// }
+
+// submitForm();
+
+
+// function Foo() {}
+// Foo.prototype.bar = function() {
+//     return 'I am bar';
+// };
+
+// var foo = new Foo();
+// console.log(foo.bar());
+
+// Testing jquery
+// $(document).ready(function() {
+//     $("hi-tech").click(function() {
+//         $("ul li").text(function(index) {
+//             console.log("Jquery Testing");
+//             return "Item Number" + (index + 1);
+//         });
+//     });
+// });
